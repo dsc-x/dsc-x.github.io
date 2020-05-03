713b37bf7fd663b8b5b3b61d37eb754246330e59
xprilion <xprilion@gmail.com>
2020-05-03 20:14:11 +0530

    Updates

---
+++ precache-manifest.2896c7a8f39dc5a3eb49a483087bfd27.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cde7023223993588e37cddfc2337da0",
+    "url": "/dscindia/index.html"
+  },
+  {
+    "revision": "a79f39dbb185e9c8615d",
+    "url": "/dscindia/static/css/main.00a8c48a.chunk.css"
+  },
+  {
+    "revision": "5119f4531f9d488309e9",
+    "url": "/dscindia/static/js/2.0d621012.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/dscindia/static/js/2.0d621012.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a79f39dbb185e9c8615d",
+    "url": "/dscindia/static/js/main.ca9da196.chunk.js"
+  },
+  {
+    "revision": "586c72ab3d367cb02b02",
+    "url": "/dscindia/static/js/runtime-main.ab19dfdd.js"
+  },
+  {
+    "revision": "04f7ed197adbb4d6b355370b318f90f6",
+    "url": "/dscindia/static/media/cardImage.04f7ed19.png"
+  },
+  {
+    "revision": "cc949774e2b8b0ac9baad375c2f98944",
+    "url": "/dscindia/static/media/dsclogo.cc949774.png"
+  },
+  {
+    "revision": "8fb0d5d99c4d1a2c2ea850290ece36e4",
+    "url": "/dscindia/static/media/shortlogo.8fb0d5d9.png"
+  }
+]);
