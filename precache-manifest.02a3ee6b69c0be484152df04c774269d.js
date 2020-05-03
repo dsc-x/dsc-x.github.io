9c27820758697b292a06a910e925174ff99fc449
xprilion <xprilion@gmail.com>
2020-05-03 21:26:49 +0530

    Updates

---
+++ precache-manifest.02a3ee6b69c0be484152df04c774269d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d69e10ddb62100eadba541167a2302",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d76bf1e5cdd957c78d63",
+    "url": "./static/css/main.5cd9c76b.chunk.css"
+  },
+  {
+    "revision": "0093333011a99b5e330b",
+    "url": "./static/js/2.1fef8499.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "./static/js/2.1fef8499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d76bf1e5cdd957c78d63",
+    "url": "./static/js/main.5002d2a2.chunk.js"
+  },
+  {
+    "revision": "690aec967630b2266da5",
+    "url": "./static/js/runtime-main.0acada96.js"
+  },
+  {
+    "revision": "04f7ed197adbb4d6b355370b318f90f6",
+    "url": "./static/media/cardImage.04f7ed19.png"
+  },
+  {
+    "revision": "cc949774e2b8b0ac9baad375c2f98944",
+    "url": "./static/media/dsclogo.cc949774.png"
+  },
+  {
+    "revision": "8fb0d5d99c4d1a2c2ea850290ece36e4",
+    "url": "./static/media/shortlogo.8fb0d5d9.png"
+  }
+]);
